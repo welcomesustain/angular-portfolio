09a6cf0fe95b4584b4f4d19fdc291e6027d2d1b2
welcomesustain <45425187+welcomesustain@users.noreply.github.com>
2021-04-09 18:14:52 +0200

    changes agn

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-portfolio'));
+app.use(express.static(__dirname + '/dist/Angular-Portfolio'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-portfolio/index.html'));});
+'/dist/Angular-Portfolio/index.html'));});
 app.listen(process.env.PORT || 8080);
